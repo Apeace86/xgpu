f3b106c6ce34865c332164cdb89160b79414204b
Apeace86 <90044141+Apeace86@users.noreply.github.com>
2023-11-03 08:10:12 +0700

    Update Vast2.sh

--- Vast2.sh
+++ Vast2.sh
@@ -65,27 +65,3 @@ echo "---------------------------"
 sudo nohup ./xengpuminer -d1 > xengpuminer-1.log 2>&1 &
 sleep 5
 echo "---------------------------"
-
-sudo nohup ./xengpuminer -d2 > xengpuminer-2.log 2>&1 &
-sleep 5
-echo "---------------------------"
-
-sudo nohup ./xengpuminer -d3 > xengpuminer-3.log 2>&1 &
-sleep 5
-echo "---------------------------"
-
-sudo nohup ./xengpuminer -d4 > xengpuminer-4.log 2>&1 &
-sleep 5
-echo "---------------------------"
-
-sudo nohup ./xengpuminer -d5 > xengpuminer-5.log 2>&1 &
-sleep 5
-echo "---------------------------"
-
-sudo nohup ./xengpuminer -d6 > xengpuminer-6.log 2>&1 &
-sleep 5
-echo "---------------------------"
-
-sudo nohup ./xengpuminer -d7 > xengpuminer-7.log 2>&1 &
-sleep 5
-echo "---------------------------"
